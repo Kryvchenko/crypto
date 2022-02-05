5f93a0627dae92b419000e271a28493276dff8e0
Oleks Kryo <amzkryvchenko@gmail.com>
2022-02-05 10:23:05 -0400

    animation added

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,25 +1,54 @@
 import React from "react";
 import "components/Navbar/MenuItems.scss";
+import { motion } from "framer-motion";
 
 export const MenuItems = (props) => {
+  const animateFrom = { opacity: 0, y: -40 };
+  const animateTo = { opacity: 1, y: 0 };
+
   return (
     <>
       <ul className="menu">
-        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+        <motion.li
+          initial={animateFrom}
+          animate={animateTo}
+          transition={{ delay: 0.05 }}
+          onClick={() => props.isMobile && props.closeMobileMenu()}
+        >
           <a href="#">Home</a>
-        </li>
-        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+        </motion.li>
+        <motion.li
+          initial={animateFrom}
+          animate={animateTo}
+          transition={{ delay: 0.1 }}
+          onClick={() => props.isMobile && props.closeMobileMenu()}
+        >
           <a href="#">Features</a>
-        </li>
-        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+        </motion.li>
+        <motion.li
+          initial={animateFrom}
+          animate={animateTo}
+          transition={{ delay: 0.2 }}
+          onClick={() => props.isMobile && props.closeMobileMenu()}
+        >
           <a href="#">Blog</a>
-        </li>
-        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+        </motion.li>
+        <motion.li
+          initial={animateFrom}
+          animate={animateTo}
+          transition={{ delay: 0.3 }}
+          onClick={() => props.isMobile && props.closeMobileMenu()}
+        >
           <a href="#">FAQ</a>
-        </li>
-        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+        </motion.li>
+        <motion.li
+          initial={animateFrom}
+          animate={animateTo}
+          transition={{ delay: 0.4 }}
+          onClick={() => props.isMobile && props.closeMobileMenu()}
+        >
           <a href="#">About & Contact</a>
-        </li>
+        </motion.li>
       </ul>
     </>
   );
