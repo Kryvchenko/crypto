1e470764503a9b9dad2385579104e625965742e6
Oleks Kryo <amzkryvchenko@gmail.com>
2022-02-16 18:31:58 -0400

    stales for table added

--- src/components/StatisticSection /Table.js
+++ src/components/StatisticSection /Table.js
@@ -6,36 +6,42 @@ export const Table = () => {
   const coins = [
     {
       name: "Bitcoin",
+      image: "images/crypto-bitcoin.png",
       price: "45 000",
       volume: "10000000",
       change: "+5%",
     },
     {
       name: "Etherium",
+      image: "images/crypto-bitcoin.png",
       price: "3000",
       volume: "2323123",
       change: "+3%",
     },
     {
       name: "Litecoin",
+      image: "images/crypto-bitcoin.png",
       price: "130",
       volume: "2232344",
       change: "+0.7%",
     },
     {
       name: "Matic",
+      image: "images/crypto-bitcoin.png",
       price: "1.87",
       volume: "223235544",
       change: "+4.26%",
     },
     {
       name: "Doge",
+      image: "images/crypto-bitcoin.png",
       price: "0.15",
       volume: "212232544",
       change: "+1.6%",
     },
     {
       name: "Miota",
+      image: "images/crypto-bitcoin.png",
       price: "0.90",
       volume: "54342323",
       change: "+5%",
@@ -46,6 +52,7 @@ export const Table = () => {
     return (
       <tr key={index} style={{ color: "#141b50" }}>
         <td>{coins.name}</td>
+        <td>{coins.image}</td>
         <td>{coins.price}</td>
         <td>{coins.volume}</td>
         <td>{coins.change}</td>
@@ -57,7 +64,7 @@ export const Table = () => {
     <>
       <ReactBootStrap.Table responsive="sm" bordered hover>
         <thead>
-          <tr>
+          <tr style={{ color: "#141b50" }}>
             <th>Name</th>
             <th>Price (USD)</th>
             <th>Volume (24h)</th>
