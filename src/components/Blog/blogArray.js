6487fd91fd6d966c4361c031131ef7219206b956
Oleks Kryo <amzkryvchenko@gmail.com>
2022-02-16 11:09:58 -0400

    test changes

--- src/components/Blog/blogArray.js
+++ src/components/Blog/blogArray.js
@@ -1,21 +1,21 @@
 export const blogArray = [
   {
     id: 1,
-    image: "/images/crypto-1.jpg",
+    image: "images/crypto-1.jpg",
     date: "25 June 2021",
     title: "How To Earn Free Cryptocurrency By Blogging",
     text: "A cryptocurrency is a digital asset designed to work as a medium of exchange using cryptography to secure the transactions.",
   },
   {
     id: 2,
-    image: "/images/crypto-2.jpg",
+    image: "images/crypto-2.jpg",
     date: "5 July 2021",
     title: "Simple Ways To Get Involved In Crypto",
     text: "Welcome to Part 2, you guys are in for a second dose of  treats today as we sat down again with Michael Taggart for a great discussion.",
   },
   {
     id: 3,
-    image: "/images/crypto-3.jpg",
+    image: "images/crypto-3.jpg",
     date: "1 Dec 2021",
     title: "A SteemIt Game Plan",
     text: "I'm obviously very very new to SteemIt. I've been posting every single day and I've been trying to post highly quality content.",
